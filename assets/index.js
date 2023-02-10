3a53fe4cdde96156862cacf704872a74865115fc
Dalton Lobosky <d.lobosky@gmail.com>
2023-02-09 23:58:13 -0600

    Separates for switch attempt.

--- assets/index.js
+++ assets/index.js
@@ -8,101 +8,210 @@ import { harmonicTremoloPedal } from './src/pedals/harmonic-tremolo.js';
 import { boostPedal } from './src/pedals/boost.js';
 import { compressorPedal } from './src/pedals/compressor.js';
 import { overdrivePedal } from './src/pedals/overdrive.js';
-import { chorusPedal } from './src/pedals/chorus.js';
+//import { chorusPedal } from './src/pedals/chorus.js';
 
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var switch1 = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
-const load = async LIVE => {
-  const DEV = false;
-  window.ctx = new window.AudioContext();
-  window.$pedalboard = document.querySelector('.pedalboard');
-  const audio = document.querySelector('audio');
-  window.buffer = null;
-
-  if (!audio) {
-    return;
-  } 
-
-  const onError = (message = '') => {
-    const error = document.createElement('div');
-    error.innerHTML = message;
-    error.classList.add('error');
-    document.body.appendChild(error);
-  };
-
-  const onMidiMessage = ({ data }) => {
-    if (data[0] === 144) {
-      window.dispatchEvent(new CustomEvent('MIDI', { detail: data[1] }));
+switch1.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
+  if (err) {
+    console.error('There was an error', err);
+  return;
+  }
+    if(value == 1) {
+      const load = async LIVE => {
+        const DEV = false;
+        window.ctx = new window.AudioContext();
+        window.$pedalboard = document.querySelector('.pedalboard');
+        const audio = document.querySelector('audio');
+        window.buffer = null;
+      
+        if (!audio) {
+          return;
+        } 
+      
+        const onError = (message = '') => {
+          const error = document.createElement('div');
+          error.innerHTML = message;
+          error.classList.add('error');
+          document.body.appendChild(error);
+        };
+      
+        const onMidiMessage = ({ data }) => {
+          if (data[0] === 144) {
+            window.dispatchEvent(new CustomEvent('MIDI', { detail: data[1] }));
+          }
+      
+          if (data[0] === 176) {
+            window.dispatchEvent(new CustomEvent('MIDIEXP', { detail: data[2] }));
+          }
+        };
+      
+        await fetch('/assets/Conic Long Echo Hall.wav')
+          .then(response => response.arrayBuffer())
+          .then(data => {
+            return ctx.decodeAudioData(data, b => {
+              buffer = b;
+            });
+          })
+          .catch(e => onError('Failed to load reverb impulse'));
+      
+        try {
+          const midiCtx = await navigator.requestMIDIAccess();
+      
+          midiCtx.inputs.forEach(entry => {
+            entry.onmidimessage = onMidiMessage;
+          });
+        } catch (e) {
+          console.log('No midi connectivity');
+        }
+      
+        let stream;
+        if (LIVE) {
+          stream = await navigator.mediaDevices
+            .getUserMedia({
+              audio: true,
+              video: false
+            })
+            .catch(e => onError("Couldn't connect to your microphone 😔"));
+        }
+      
+        let source;
+      
+        if (LIVE) {
+          source = ctx.createMediaStreamSource(stream);
+        } else {
+          source = ctx.createMediaElementSource(audio);
+          audio.muted = false;
+          audio.volume = DEV ? 0 : 1;
+          audio.currentTime = 2;
+          audio.play();
+        }
+      
+        const pedals = [
+          //wahPedal,
+          //compressorPedal,
+          overdrivePedal,
+          //boostPedal,
+          //harmonicTremoloPedal,
+          //chorusPedal,
+          delayPedal,
+          //multiHeadDelay,
+          //tremoloPedal,
+          reverbPedal
+        ];
+      
+        if (window.MediaRecorder !== undefined) {
+          pedals.push(loopPedal)
+        }
+      
+        const output = pedals.reduce((input, pedal, index) => {
+          return pedal(input, index + 1);
+        }, source);
+      
+        output.connect(ctx.destination);
+      };
+    }
+    //================================================================================================================================================================
+    if(value == 0){
+      const load = async LIVE => {
+        const DEV = false;
+        window.ctx = new window.AudioContext();
+        window.$pedalboard = document.querySelector('.pedalboard');
+        const audio = document.querySelector('audio');
+        window.buffer = null;
+      
+        if (!audio) {
+          return;
+        } 
+      
+        const onError = (message = '') => {
+          const error = document.createElement('div');
+          error.innerHTML = message;
+          error.classList.add('error');
+          document.body.appendChild(error);
+        };
+      
+        const onMidiMessage = ({ data }) => {
+          if (data[0] === 144) {
+            window.dispatchEvent(new CustomEvent('MIDI', { detail: data[1] }));
+          }
+      
+          if (data[0] === 176) {
+            window.dispatchEvent(new CustomEvent('MIDIEXP', { detail: data[2] }));
+          }
+        };
+      
+        await fetch('/assets/Conic Long Echo Hall.wav')
+          .then(response => response.arrayBuffer())
+          .then(data => {
+            return ctx.decodeAudioData(data, b => {
+              buffer = b;
+            });
+          })
+          .catch(e => onError('Failed to load reverb impulse'));
+      
+        try {
+          const midiCtx = await navigator.requestMIDIAccess();
+      
+          midiCtx.inputs.forEach(entry => {
+            entry.onmidimessage = onMidiMessage;
+          });
+        } catch (e) {
+          console.log('No midi connectivity');
+        }
+      
+        let stream;
+        if (LIVE) {
+          stream = await navigator.mediaDevices
+            .getUserMedia({
+              audio: true,
+              video: false
+            })
+            .catch(e => onError("Couldn't connect to your microphone 😔"));
+        }
+      
+        let source;
+      
+        if (LIVE) {
+          source = ctx.createMediaStreamSource(stream);
+        } else {
+          source = ctx.createMediaElementSource(audio);
+          audio.muted = false;
+          audio.volume = DEV ? 0 : 1;
+          audio.currentTime = 2;
+          audio.play();
+        }
+      
+        const pedals = [
+          //wahPedal,
+          compressorPedal,
+          //overdrivePedal,
+          //boostPedal,
+          //harmonicTremoloPedal,
+          chorusPedal,
+          //delayPedal,
+          //multiHeadDelay,
+          tremoloPedal,
+          //reverbPedal
+        ];
+      
+        if (window.MediaRecorder !== undefined) {
+          pedals.push(loopPedal)
+        }
+      
+        const output = pedals.reduce((input, pedal, index) => {
+          return pedal(input, index + 1);
+        }, source);
+      
+        output.connect(ctx.destination);
+      };
     }
 
-    if (data[0] === 176) {
-      window.dispatchEvent(new CustomEvent('MIDIEXP', { detail: data[2] }));
-    }
-  };
+});
 
-  await fetch('/assets/Conic Long Echo Hall.wav')
-    .then(response => response.arrayBuffer())
-    .then(data => {
-      return ctx.decodeAudioData(data, b => {
-        buffer = b;
-      });
-    })
-    .catch(e => onError('Failed to load reverb impulse'));
+//================================================================================================================================================================
 
-  try {
-    const midiCtx = await navigator.requestMIDIAccess();
-
-    midiCtx.inputs.forEach(entry => {
-      entry.onmidimessage = onMidiMessage;
-    });
-  } catch (e) {
-    console.log('No midi connectivity');
-  }
-
-  let stream;
-  if (LIVE) {
-    stream = await navigator.mediaDevices
-      .getUserMedia({
-        audio: true,
-        video: false
-      })
-      .catch(e => onError("Couldn't connect to your microphone 😔"));
-  }
-
-  let source;
-
-  if (LIVE) {
-    source = ctx.createMediaStreamSource(stream);
-  } else {
-    source = ctx.createMediaElementSource(audio);
-    audio.muted = false;
-    audio.volume = DEV ? 0 : 1;
-    audio.currentTime = 2;
-    audio.play();
-  }
-
-  const pedals = [
-    wahPedal,
-    compressorPedal,
-    overdrivePedal,
-    boostPedal,
-    harmonicTremoloPedal,
-    chorusPedal,
-    delayPedal,
-    multiHeadDelay,
-    tremoloPedal,
-    reverbPedal
-  ];
-
-  if (window.MediaRecorder !== undefined) {
-    pedals.push(loopPedal)
-  }
-
-  const output = pedals.reduce((input, pedal, index) => {
-    return pedal(input, index + 1);
-  }, source);
-
-  output.connect(ctx.destination);
-};
 
 (() => {
   const getStarted = ({ target }) => {
